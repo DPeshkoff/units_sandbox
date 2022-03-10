1f3becc9fad10fbf40a5fea98f531d37504a33e9
DPeshkoff <dmytrypeshkov@gmail.com>
2022-03-10 20:30:14 +0300

    write-tests: написать тесты

--- src/isMono.js
+++ src/isMono.js
@@ -4,7 +4,29 @@
  *  isMono([0, 1, 5, 9, 15]) вернет true
  *  isMono([0, 1, 1]) вернет true
  *  isMono([0, 1, 0]) вернет false
+ * @param {Array} values массив значений
+ * @returns {boolean} монотонность
  */
-export function isMono(values) {
+export const isMono = (values) => {
+    // Не массив -> false
+    if (!Array.isArray(values)) {
+        return false;
+    }
+
+    // Длина 1 или 0 -> true
+    if (values.length <= 1) {
+        return true;
+    }
+
+    const isAscending = values[0] < values[values.length - 1] ? true : false;
+
+    return values.every((item, index) => {
+        if (!values[index + 1] ||
+            !isAscending && item >= values[index + 1] ||
+            isAscending && item <= values[index + 1]) {
+            return true;
+        }
+        return false;
+    });
 
 }
--- src/isMono.test.js
+++ src/isMono.test.js
@@ -9,6 +9,66 @@ describe('test isMono function', () => {
     assert.strictEqual(result, true);
   });
 
+  test('[3, 2, 3]', () => {
+    const result = isMono([3, 2, 3]);
+
+    assert.strictEqual(result, false);
+  });
+
+  test('[1.2, 1.3, 1.4]', () => {
+    const result = isMono([1.2, 1.3, 1.4]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[0]', () => {
+    const result = isMono([0]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[-Infinity, Infinity]', () => {
+    const result = isMono([-Infinity, Infinity]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[]', () => {
+    const result = isMono([]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('["1", "2", "3"]', () => {
+    const result = isMono(["1", "2", "3"]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('"notArray"', () => {
+    const result = isMono('notArray');
+
+    assert.strictEqual(result, false);
+  });
+
+  test('[1, 1, 3]', () => {
+    const result = isMono([1, 1, 3]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[-1, -2, -3, -5]', () => {
+    const result = isMono([-1, -2, -3, -5]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[-4, -2, -3]', () => {
+    const result = isMono([-4, -2, -3]);
+
+    assert.strictEqual(result, false);
+  });
+
   // Добавьте сюда ваши тесты
 })
 
